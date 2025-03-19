d6fb39b6414f73b7e27e50e12d23f04d9b1406f0
Sam Park <samp1800@live.com>
2025-03-18 20:05:02 -0400

    commit package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='Demo',
+	version='0.1',
+	description='A simple demonstration package',
+	url='',
+	author='Sam Park',
+	author_email='',
+	license='MIT',
+	packages=['demo']
+)
